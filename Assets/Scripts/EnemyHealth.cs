a06a7ff0700c329f81270d3316248c5bbb271b83
Trung Ly <Jun.Ly@dal.ca>
2025-11-07 09:44:36 -0400

    Enhance enemy health management with animations for taking damage and death

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,40 +2,64 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Public variable to set in the Inspector
     public int maxHealth = 3;
+    public float deathAnimationTime = 1f; // How long is the death animation?
     
-    // Private variable to track current health
     private int currentHealth;
+    private Animator animator; // NEW: A reference to the Animator
+    private bool isDead = false; // NEW: A flag to stop multiple deaths
 
-    // Start is called when the game begins
     void Start()
     {
-        // Set the health to full when the enemy spawns
         currentHealth = maxHealth;
+        
+        // NEW: Get the Animator component on this enemy
+        animator = GetComponent<Animator>(); 
     }
 
-    // This is a public function that our hitbox can call
     public void TakeDamage(int damage)
     {
-        // Subtract the damage from our health
-        currentHealth -= damage;
+        if (isDead)
+        {
+            return;
+        }
 
+        currentHealth -= damage;
         Debug.Log("Enemy took damage! Current health: " + currentHealth);
 
-        // Check if the enemy should be destroyed
+        // This is your new logic
         if (currentHealth <= 0)
         {
             Die();
+        }
+        else
+        {
+            // --- NEW LINE ---
+            // If we didn't die, play the take hit animation
+            animator.SetTrigger("TakeHit");
+            // --- END NEW LINE ---
         }
     }
 
     void Die()
     {
-        // For now, we just print a message and destroy the GameObject
+        // NEW: This function is now completely different
+
+        // 1. Set the isDead flag so this can't be called again
+        isDead = true;
         Debug.Log("Enemy has been defeated!");
+
+        // 2. Tell the Animator to play the "Death" animation
+        animator.SetTrigger("Death");
+
+        // 3. Disable the enemy's collider so it can't be hit anymore
+        GetComponent<Collider2D>().enabled = false;
         
-        // This removes the enemy from the game scene
-        Destroy(gameObject);
+        // (Optional) If you add a movement script, you'd disable it here too
+        // GetComponent<EnemyMovement>().enabled = false;
+
+        // 4. Destroy this enemy GameObject, but *wait* for the animation to finish
+        // The 'deathAnimationTime' should be the length (in seconds) of your death clip
+        Destroy(gameObject, deathAnimationTime);
     }
 }
